ffd0c2c7784fba77d3e6cadd8a7ebcc0ea516a3e
vyun <vyun@uwaterloo.ca>
2019-12-31 16:58:52 -0500

    webhook works w validation and messages

--- webhook.js
+++ webhook.js
@@ -4,35 +4,18 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const server = app.listen(process.env.PORT || 1337, () => console.log('webhook server is now listening on port 1337 in %s mode', app.settings.env));
+const server = app.listen(process.env.PORT || 1337, () => {
+  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
 
-// basic fb validation
 app.get('/webhook', (req, res) => {
   if (req.query['hub.mode'] && req.query['hub.verify_token'] === 'funmblr') {
-  	console.log('WEBHOOK_VERIFIED');
     res.status(200).send(req.query['hub.challenge']);
   } else {
     res.status(403).end();
   }
 });
 
-// app.get('/webhook', (req, res) => {
-//   let VERIFY_TOKEN = "funmblr"
-//   let mode = req.query['hub.mode'];
-//   let token = req.query['hub.verify_token'];
-//   let challenge = req.query['hub.challenge'];
-    
-//   if (mode && token) {
-//     if (mode && token === VERIFY_TOKEN) {
-//       console.log('WEBHOOK_VERIFIED');
-//       res.status(200).send(challenge);
-//     } else {
-//       res.sendStatus(403).end();      
-//     }
-//   }
-// });
-
-// for messages
 app.post('/webhook', (req, res) => {
   console.log(req.body);
   if (req.body.object === 'page') {
