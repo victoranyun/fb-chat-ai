4e38af15571caa256640e4903b276e3795c9e466
vyun <vyun@uwaterloo.ca>
2019-12-30 22:03:03 -0500

    webhook behaving weirdly with fb

--- webhook.js
+++ webhook.js
@@ -1,20 +1,36 @@
-'use strict';
-
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express().use(bodyParser.json());
+const app = express();
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(process.env.PORT || 1337, () => console.log('webhook server is now listening on port 1337 in %s mode', app.settings.env));
+const server = app.listen(process.env.PORT || 1337, () => console.log('webhook server is now listening on port 1337 in %s mode', app.settings.env));
 
 // basic fb validation
 app.get('/webhook', (req, res) => {
-  if (req.query['hub.mode'] && req.query['hub.verify_token'] === 'tuxedo_cat') {
+  if (req.query['hub.mode'] && req.query['hub.verify_token'] === 'funmblr') {
+  	console.log('WEBHOOK_VERIFIED');
     res.status(200).send(req.query['hub.challenge']);
   } else {
     res.status(403).end();
   }
 });
+
+// app.get('/webhook', (req, res) => {
+//   let VERIFY_TOKEN = "funmblr"
+//   let mode = req.query['hub.mode'];
+//   let token = req.query['hub.verify_token'];
+//   let challenge = req.query['hub.challenge'];
+    
+//   if (mode && token) {
+//     if (mode && token === VERIFY_TOKEN) {
+//       console.log('WEBHOOK_VERIFIED');
+//       res.status(200).send(challenge);
+//     } else {
+//       res.sendStatus(403).end();      
+//     }
+//   }
+// });
 
 // for messages
 app.post('/webhook', (req, res) => {
